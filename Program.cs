70da76f6258a16ba17938fdf1de8364ed4756833
Adam Zivlak <Adam.zivlak99@hotmail.se>
2023-02-24 09:17:39 +0100

    Klass "Element" skapad, med de publika attriubuten grundämne, z (atomnummer), typ, smältpunkt, kokpunkt

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace D15_Inlämningsuppgift_3
+{
+    internal class Program
+    {
+        public class Element
+        {
+            public string grundämne, typ;
+            public int atomnummer;
+            public double smältpunkt, kokpunkt;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
